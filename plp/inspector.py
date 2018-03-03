48654adc10e337c8f75f63b16919179700c7a3b4
David Grant <david.grant@physics.ox.ac.uk>
2018-03-03 17:52:05 +0000

    Create inpector class.

---
+++ plp/inspector.py
@@ -0,0 +1,9 @@
+class DataInspector(object):
+    """ Inspector for conducting pre-processing data checks. """
+
+    def __init__(self, verbose=False):
+        self.verbose = verbose
+
+    def __repr__(self):
+        """ String repr of object. """
+        return 'Pre-processing data inspector object.'
